9492a8f90ae992a1b6d8beb8bafa9717993e8a64
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2020-03-05 16:44:38 +0900

    Live update!!

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -6,6 +6,7 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
+      <p>Live update!!</p>
       <strong>Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
